847e3907267be0ed02f3a7429db72ab56b4e7fce
Angela <angela.mihajlovska1@gmail.com>
2024-05-14 12:13:49 +0200

    Initial commit

---
+++ src/registration.tsx
@@ -0,0 +1,147 @@
+import React, { ChangeEvent, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Registration = () => {
+  const [valueName, setValueName] = useState("");
+  const [valuePrintName, setValuePrintName] = useState("");
+
+  function printName(eventName: ChangeEvent<HTMLInputElement>) {
+    setValueName(eventName.target.value);
+    console.log(eventName);
+    console.log(valueName);
+  }
+
+  function handleOnClick(event: any) {
+    event.preventDefault(event);
+    setValuePrintName(valueName);
+    setValuePrintEmail(valueMail);
+    setValuePrintPass(valuePass);
+    setValuePrintConfirmPass(valuePrintConfirmPass);
+  }
+
+  const [valueMail, setValueMail] = useState("");
+  const [valuePrintEmail, setValuePrintEmail] = useState("");
+
+  function printEmail(eventMail: ChangeEvent<HTMLInputElement>) {
+    setValueMail(eventMail.target.value);
+    console.log("event1:", eventMail);
+    console.log(valueMail);
+  }
+
+  const [valuePass, setValuePass] = useState("");
+  const [valuePrintPass, setValuePrintPass] = useState("");
+
+  function printPass(eventPass: ChangeEvent<HTMLInputElement>) {
+    setValuePass(eventPass.target.value);
+    console.log("event1:", eventPass);
+    console.log(valuePass);
+  }
+
+  const [valueConfirmPass, setValueConfirmPass] = useState("");
+  const [valuePrintConfirmPass, setValuePrintConfirmPass] = useState("");
+
+  function printConfirmPass(eventConfirmPass: ChangeEvent<HTMLInputElement>) {
+    setValueConfirmPass(eventConfirmPass.target.value);
+    console.log("event1:", eventConfirmPass);
+    console.log(eventConfirmPass);
+  }
+
+  return (
+    <div className="row d-flex justify-content-center align-items-center vh-100 bg-body-secondary">
+      <form className="col-6">
+        <div className="mb-3 row justify-content-end">
+          <label
+            htmlFor="inputName"
+            className="col-sm-3 col-form-label text-end"
+          >
+            Name
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              className="form-control"
+              id="inputName"
+              aria-describedby="nameHelp"
+              required
+              value={valueName}
+              onChange={(eventName) => printName(eventName)}
+            />
+          </div>
+        </div>
+        <div className="mb-3 row justify-content-end">
+          <label
+            htmlFor="inputEmail"
+            className="col-sm-3 col-form-label text-end"
+          >
+            Email address
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              required
+              value={valueMail}
+              onChange={(eventMail) => printEmail(eventMail)}
+            />
+          </div>
+        </div>
+        <div className="mb-3 row justify-content-end">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-3 col-form-label text-end"
+          >
+            Password
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              required
+              value={valuePass}
+              onChange={(eventPass) => printPass(eventPass)}
+            />
+          </div>
+        </div>
+
+        <div className="mb-3 row justify-content-end">
+          <label
+            htmlFor="inputConfirmPassword"
+            className="col-sm-3 col-form-label text-end"
+          >
+            Confirm Password
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="password"
+              className="form-control"
+              id="inputConfirmPassword"
+              required
+              value={valueConfirmPass}
+              onChange={(eventConfirmPass) =>
+                printConfirmPass(eventConfirmPass)
+              }
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary justify-content-center "
+          onClick={(event) => handleOnClick(event)}
+        >
+          <a href="">Add user</a>
+        </button>
+      </form>
+
+      <div className="Printed-text">{valuePrintName}</div>
+      <div className="Printed-text">{valuePrintEmail}</div>
+      <div className="Printed-text">{valuePrintPass}</div>
+      <div className="Printed-text">{valuePrintConfirmPass}</div>
+    </div>
+  );
+};
+
+export default Registration;
