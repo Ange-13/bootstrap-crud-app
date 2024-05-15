b276aaf299d79349d5cee987a1ca7fb0fdce7e4a
Angela <angela.mihajlovska1@gmail.com>
2024-05-15 11:49:04 +0200

    Added a table below the form

--- src/components/registration.tsx
+++ src/components/registration.tsx
@@ -1,9 +1,9 @@
-import React, { ChangeEvent, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { ChangeEvent, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Registration = () => {
-  const [valueName, setValueName] = useState('');
-  const [valuePrintName, setValuePrintName] = useState('');
+  const [valueName, setValueName] = useState("");
+  const [valuePrintName, setValuePrintName] = useState("");
 
   function printName(eventName: ChangeEvent<HTMLInputElement>) {
     setValueName(eventName.target.value);
@@ -13,11 +13,11 @@ const Registration = () => {
 
   function handleOnClick(event: any) {
     event.preventDefault(event);
-    fetch('http://localhost:8081/users', {
-      method: 'POST',
+    fetch("http://localhost:8081/users", {
+      method: "POST",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: valueName,
@@ -32,51 +32,49 @@ const Registration = () => {
     });
   }
 
-  const [valueMail, setValueMail] = useState('');
-  const [valuePrintEmail, setValuePrintEmail] = useState('');
+  const [valueMail, setValueMail] = useState("");
+  const [valuePrintEmail, setValuePrintEmail] = useState("");
 
   function printEmail(eventMail: ChangeEvent<HTMLInputElement>) {
     setValueMail(eventMail.target.value);
-    console.log('event1:', eventMail);
+    console.log("event1:", eventMail);
     console.log(valueMail);
   }
 
-  const [valuePass, setValuePass] = useState('');
-  const [valuePrintPass, setValuePrintPass] = useState('');
+  const [valuePass, setValuePass] = useState("");
+  const [valuePrintPass, setValuePrintPass] = useState("");
 
   function printPass(eventPass: ChangeEvent<HTMLInputElement>) {
     setValuePass(eventPass.target.value);
-    console.log('event1:', eventPass);
+    console.log("event1:", eventPass);
     console.log(valuePass);
   }
 
-  const [valueConfirmPass, setValueConfirmPass] = useState('');
-  const [valuePrintConfirmPass, setValuePrintConfirmPass] = useState('');
+  const [valueConfirmPass, setValueConfirmPass] = useState("");
+  const [valuePrintConfirmPass, setValuePrintConfirmPass] = useState("");
 
   function printConfirmPass(eventConfirmPass: ChangeEvent<HTMLInputElement>) {
     setValueConfirmPass(eventConfirmPass.target.value);
-    console.log('event1:', eventConfirmPass);
+    console.log("event1:", eventConfirmPass);
     console.log(eventConfirmPass);
   }
 
   return (
-    <div className='row d-flex justify-content-center align-items-center vh-100 bg-body-secondary'>
-      <form className='col-6'>
-        {/* Form fields */}
-        <div className='mb-3 row justify-content-end'>
-          {/* Name */}
+    <div className="row d-flex justify-content-center align-items-center vh-100 bg-body-secondary">
+      <form className="col-6">
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputName'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputName"
+            className="col-sm-3 col-form-label text-end"
           >
             Name
           </label>
-          <div className='col-sm-9'>
+          <div className="col-sm-9">
             <input
-              type='text'
-              className='form-control'
-              id='inputName'
-              aria-describedby='nameHelp'
+              type="text"
+              className="form-control"
+              id="inputName"
+              aria-describedby="nameHelp"
               required
               value={valueName}
               onChange={(eventName) => printName(eventName)}
@@ -84,19 +82,19 @@ const Registration = () => {
           </div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputEmail'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputEmail"
+            className="col-sm-3 col-form-label text-end"
           >
             Email address
           </label>
-          <div className='col-sm-9'>
+          <div className="col-sm-9">
             <input
-              type='email'
-              className='form-control'
-              id='inputEmail'
-              aria-describedby='emailHelp'
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
               required
               value={valueMail}
               onChange={(eventMail) => printEmail(eventMail)}
@@ -104,18 +102,18 @@ const Registration = () => {
           </div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputPassword'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputPassword"
+            className="col-sm-3 col-form-label text-end"
           >
             Password
           </label>
-          <div className='col-sm-9'>
+          <div className="col-sm-9">
             <input
-              type='password'
-              className='form-control'
-              id='inputPassword'
+              type="password"
+              className="form-control"
+              id="inputPassword"
               required
               value={valuePass}
               onChange={(eventPass) => printPass(eventPass)}
@@ -123,18 +121,18 @@ const Registration = () => {
           </div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputConfirmPassword'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputConfirmPassword"
+            className="col-sm-3 col-form-label text-end"
           >
             Confirm Password
           </label>
-          <div className='col-sm-9'>
+          <div className="col-sm-9">
             <input
-              type='password'
-              className='form-control'
-              id='inputConfirmPassword'
+              type="password"
+              className="form-control"
+              id="inputConfirmPassword"
               required
               value={valueConfirmPass}
               onChange={(eventConfirmPass) =>
@@ -145,18 +143,48 @@ const Registration = () => {
         </div>
 
         <button
-          type='submit'
-          className='btn btn-primary justify-content-center '
+          type="submit"
+          className="btn btn-primary justify-content-center "
           onClick={(event) => handleOnClick(event)}
         >
           Add user
         </button>
       </form>
 
-      <div className='d-inline-block'>
-        {valuePrintName} {valuePrintEmail} {valuePrintPass}{' '}
-        {valuePrintConfirmPass}
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>{valuePrintName}</td>
+            <td>{valuePrintEmail}</td>
+            <td>{valuePrintPass}</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Newuser</td>
+            <td>newuser@mail.com</td>
+            <td>NewUser123</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Maria</td>
+            <td>maria@gmail.com</td>
+            <td>Maria123</td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* <div className="d-inline-block">
+        {valuePrintEmail} {valuePrintPass} {valuePrintConfirmPass}
+      </div> */}
     </div>
   );
 };
