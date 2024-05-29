d2cf4415d77bfd34d835d34504f16386f1b3fcc4
Ange-13 <angela.mihajlovska1@gmail.com>
2024-05-29 16:21:12 +0200

    Added some minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,92 +1,92 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8081;
-const cors = require('cors');
-const { Client } = require('pg');
+const cors = require("cors");
+const { Client } = require("pg");
 
 app.use(express.json());
-// Use the cors middleware
+
 app.use(cors());
 
 const client = new Client({
-  user: 'postgres',
-  password: 'postgres',
-  host: 'localhost',
-  port: '5432',
-  database: 'users',
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: "5432",
+  database: "users",
 });
 
 client
   .connect(null)
   .then(() => {
-    console.log('Connected to PostgreSQL database');
+    console.log("Connected to PostgreSQL database");
   })
   .catch((err) => {
-    console.error('Error connecting to PostgreSQL database', err);
+    console.error("Error connecting to PostgreSQL database", err);
   });
 
 const exampleUser = {
-  name: 'Ange',
-  email: 'ange@mgmail.com',
-  pass: 'pass',
-  confirmPass: 'pass',
+  name: "Ange",
+  email: "ange@mgmail.com",
+  pass: "pass",
+  confirmPass: "pass",
 };
 
-app.get('/users', (req, res) => {
-  client.query('SELECT * FROM users ORDER BY id', (err, result) => {
+app.get("/users", (req, res) => {
+  client.query("SELECT * FROM users ORDER BY id", (err, result) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.status(200).send(result.rows);
     }
   });
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   const { name, email, password } = req.body;
   const insertQuery =
-    'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)';
+    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
   const queryParams = [name, email, password];
 
   try {
     const result = await client.query(insertQuery, queryParams);
-    res.status(201).send('{}');
+    res.status(201).send("{}");
   } catch (err) {
     if (err.code == 23505) {
       res
         .status(400)
         .send({ error: `User with ${email} email address already exists` });
     } else {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     }
   }
 });
 
-app.patch('/users/:id', async (req, res) => {
+app.patch("/users/:id", async (req, res) => {
   const { name, email, password } = req.body;
   const updateQuery =
-    'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4';
+    "UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4";
   const queryParams = [name, email, password, req.params.id];
 
   try {
     const result = await client.query(updateQuery, queryParams);
-    res.status(201).send('{}');
+    res.status(201).send("{}");
   } catch (err) {
     if (err.code == 23505) {
       res.status(400).send({ error: `User with ${email} already exists` });
     } else {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     }
   }
 });
 
-app.delete('/users/:id', (req, res) => {
-  const deleteQuery = 'DELETE from users WHERE id = $1';
+app.delete("/users/:id", (req, res) => {
+  const deleteQuery = "DELETE from users WHERE id = $1";
   const queryParams = [req.params.id];
 
   client.query(deleteQuery, queryParams, (err, result) => {
     if (err) {
-      res.status(500).send('Internal Server Error');
+      res.status(500).send("Internal Server Error");
     } else {
       res.status(200).send();
     }
--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,27 +1,27 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Button } from 'react-bootstrap';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Modal, Button } from "react-bootstrap";
 
 interface User {
   id: number;
   name: string;
   email: string;
   password: string;
-  confirmPassword: '';
+  confirmPassword: "";
 }
 
 const Registration = () => {
-  const [valueName, setValueName] = useState('');
-  const [valuePrintName, setValuePrintName] = useState('');
+  const [valueName, setValueName] = useState("");
+  const [valuePrintName, setValuePrintName] = useState("");
   const [users, setUsers] = useState<User[]>([]);
   const [toggleModal, setToggleModal] = useState(false);
   const [currentUser, setCurrentUser] = useState<User>();
-  const [valuePass, setValuePass] = useState('');
-  const [valuePrintPass, setValuePrintPass] = useState('');
-  const [valueMail, setValueMail] = useState('');
-  const [valuePrintMail, setValuePrintMail] = useState('');
-  const [valueConfirmPass, setValueConfirmPass] = useState('');
-  const [valuePrintConfirmPass, setValuePrintConfirmPass] = useState('');
+  const [valuePass, setValuePass] = useState("");
+  const [valuePrintPass, setValuePrintPass] = useState("");
+  const [valueMail, setValueMail] = useState("");
+  const [valuePrintMail, setValuePrintMail] = useState("");
+  const [valueConfirmPass, setValueConfirmPass] = useState("");
+  const [valuePrintConfirmPass, setValuePrintConfirmPass] = useState("");
 
   useEffect(() => {
     fetchUsers().then();
@@ -39,29 +39,29 @@ const Registration = () => {
 
     event.preventDefault(event);
     if (valueName.length <= 8) {
-      alert('Username must contain more than 8 characters');
+      alert("Username must contain more than 8 characters");
       return;
     }
     if (valuePass.length <= 4) {
-      alert('Your password must contain more than 8 characters!');
+      alert("Your password must contain more than 8 characters!");
       return;
     }
 
     if (!hasLetters || !hasNumbers) {
-      alert('Password must contain both letters and numbers');
+      alert("Password must contain both letters and numbers");
       return;
     }
 
     if (valuePass != valueConfirmPass) {
-      alert('Passwords do not match ! Please confirm your password.');
+      alert("Passwords do not match ! Please confirm your password.");
       return;
     }
 
-    fetch('http://localhost:8081/users', {
-      method: 'POST',
+    fetch("http://localhost:8081/users", {
+      method: "POST",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: valueName,
@@ -84,10 +84,10 @@ const Registration = () => {
 
   function deleteOnClick(id: number) {
     fetch(`http://localhost:8081/users/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
     }).then(() => {
       fetchUsers().then();
@@ -96,11 +96,11 @@ const Registration = () => {
 
   async function fetchUsers() {
     try {
-      const response = await fetch('http://localhost:8081/users');
+      const response = await fetch("http://localhost:8081/users");
       const data = await response.json();
       setUsers(data);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error("Error fetching users:", error);
     }
   }
 
@@ -120,18 +120,18 @@ const Registration = () => {
       return;
     }
     if (currentUser.name.length <= 8) {
-      alert('Username must contain more than 8 characters');
+      alert("Username must contain more than 8 characters");
       return;
     }
     if (currentUser.password.length <= 4) {
-      alert('Password must be longer than 4 characters');
+      alert("Password must be longer than 4 characters");
       return;
     }
     fetch(`http://localhost:8081/users/${currentUser?.id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         name: currentUser?.name,
@@ -149,7 +149,7 @@ const Registration = () => {
 
   function printEmail(eventMail: ChangeEvent<HTMLInputElement>) {
     setValueMail(eventMail.target.value);
-    console.log('event1:', eventMail);
+    console.log("event1:", eventMail);
     console.log(valueMail);
   }
 
@@ -158,7 +158,7 @@ const Registration = () => {
       let updatedUser = { ...currentUser };
       updatedUser.name = eventName.target.value;
       setCurrentUser(updatedUser);
-      console.log('Updated user:', updatedUser);
+      console.log("Updated user:", updatedUser);
     }
   }
 
@@ -167,7 +167,7 @@ const Registration = () => {
       let updatedUser = { ...currentUser };
       updatedUser.email = eventName.target.value;
       setCurrentUser(updatedUser);
-      console.log('Updated user:', updatedUser);
+      console.log("Updated user:", updatedUser);
     }
   }
 
@@ -176,98 +176,98 @@ const Registration = () => {
       let updatedUser = { ...currentUser };
       updatedUser.password = eventName.target.value;
       setCurrentUser(updatedUser);
-      console.log('Updated user:', updatedUser);
+      console.log("Updated user:", updatedUser);
     }
   }
 
   function printPass(eventPass: ChangeEvent<HTMLInputElement>) {
     setValuePass(eventPass.target.value);
-    console.log('event1:', eventPass);
+    console.log("event1:", eventPass);
     console.log(valuePass);
   }
 
   function printConfirmPass(eventConfirmPass: ChangeEvent<HTMLInputElement>) {
     setValueConfirmPass(eventConfirmPass.target.value);
-    console.log('event1:', eventConfirmPass);
+    console.log("event1:", eventConfirmPass);
     console.log(eventConfirmPass);
   }
 
   return (
-    <div className='row d-flex  align-items-center vh-100 bg-body-secondary text-center'>
-      <form className='col-6  was-validated  needs-validation novalidate'>
-        <div className='mb-3 row justify-content-end'>
+    <div className="row d-flex  align-items-center vh-100 bg-body-secondary text-center">
+      <form className="col-6  was-validated  needs-validation novalidate">
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputName'
-            className='col-sm-3 col-form-label text-end '
+            htmlFor="inputName"
+            className="col-sm-3 col-form-label text-end "
           >
             Name
           </label>
-          <div className='col-sm-6'>
+          <div className="col-sm-6">
             <input
               required
-              type='text'
-              className='form-control '
-              id='inputName'
-              aria-describedby='nameHelp'
+              type="text"
+              className="form-control "
+              id="inputName"
+              aria-describedby="nameHelp"
               value={valueName}
               onChange={(eventName) => printName(eventName)}
             />
           </div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputEmail'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputEmail"
+            className="col-sm-3 col-form-label text-end"
           >
             Email address
           </label>
-          <div className='col-sm-6'>
+          <div className="col-sm-6">
             <input
               required
-              type='email'
-              className='form-control'
-              id='inputEmail'
-              aria-describedby='emailHelp'
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
               value={valueMail}
               onChange={(eventMail) => printEmail(eventMail)}
             />
           </div>
-          <div className='invalid-feedback'>Invalid email address</div>
+          <div className="invalid-feedback">Invalid email address</div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputPassword'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputPassword"
+            className="col-sm-3 col-form-label text-end"
           >
             Password
           </label>
-          <div className='col-sm-6'>
+          <div className="col-sm-6">
             <input
               required
-              type='password'
-              className='form-control'
-              id='inputPassword'
+              type="password"
+              className="form-control"
+              id="inputPassword"
               value={valuePass}
               onChange={(eventPass) => printPass(eventPass)}
             />
           </div>
         </div>
 
-        <div className='mb-3 row justify-content-end'>
+        <div className="mb-3 row justify-content-end">
           <label
-            htmlFor='inputConfirmPassword'
-            className='col-sm-3 col-form-label text-end'
+            htmlFor="inputConfirmPassword"
+            className="col-sm-3 col-form-label text-end"
           >
             Confirm Password
           </label>
-          <div className='col-sm-6'>
+          <div className="col-sm-6">
             <input
               required
-              type='password'
-              className='form-control'
-              id='inputConfirmPassword'
+              type="password"
+              className="form-control"
+              id="inputConfirmPassword"
               value={valueConfirmPass}
               onChange={(eventConfirmPass) =>
                 printConfirmPass(eventConfirmPass)
@@ -276,11 +276,11 @@ const Registration = () => {
           </div>
         </div>
 
-        <div className='row justify-content-end'>
-          <div className='col-sm-9 offset-sm-3 d-flex justify-content-end'>
+        <div className="row justify-content-end">
+          <div className="col-sm-9 offset-sm-3 d-flex justify-content-end">
             <button
-              type='submit'
-              className='btn btn-primary'
+              type="submit"
+              className="btn btn-primary"
               onClick={(event) => handleOnClick(event)}
             >
               Add user
@@ -289,32 +289,32 @@ const Registration = () => {
         </div>
       </form>
 
-      <table className='table table-hover'>
+      <table className="table table-hover">
         <thead>
           <tr>
-            <th scope='col'>#</th>
-            <th scope='col'>Name</th>
-            <th scope='col'>Email</th>
-            <th scope='col'>Password</th>
-            <th scope='col'>Actions</th>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Password</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <th scope='row'>{user.id}</th>
+              <th scope="row">{user.id}</th>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.password}</td>
               <td>
                 <button
-                  className='btn btn-primary btn-sm me-1'
+                  className="btn btn-primary btn-sm me-1"
                   onClick={() => showEdit(user)}
                 >
                   Edit
                 </button>
                 <button
-                  className='btn btn-danger btn-sm'
+                  className="btn btn-danger btn-sm"
                   onClick={() => {
                     deleteOnClick(user.id);
                   }}
@@ -333,60 +333,60 @@ const Registration = () => {
         </Modal.Header>
         <Modal.Body>
           <form>
-            <div className='mb-3'>
-              <label htmlFor='editName' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="editName" className="form-label">
                 Name
               </label>
               <input
-                type='text'
-                className='form-control'
-                id='editName'
+                type="text"
+                className="form-control"
+                id="editName"
                 value={currentUser?.name}
                 onChange={(eventName) => changeCurrentName(eventName)}
               />
             </div>
-            <div className='mb-3'>
-              <label htmlFor='editEmail' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="editEmail" className="form-label">
                 Email
               </label>
               <input
-                type='email'
-                className='form-control'
-                id='editEmail'
+                type="email"
+                className="form-control"
+                id="editEmail"
                 value={currentUser?.email}
                 onChange={(eventMail) => changeCurrentEmail(eventMail)}
               />
             </div>
-            <div className='mb-3'>
-              <label htmlFor='editPassword' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="editPassword" className="form-label">
                 Password
               </label>
               <input
-                type='password'
-                className='form-control'
-                id='editPassword'
+                type="password"
+                className="form-control"
+                id="editPassword"
                 value={currentUser?.password}
                 onChange={(eventPass) => changeCurrentPassword(eventPass)}
               />
             </div>
-            <div className='mb-3'>
-              <label htmlFor='confirmPassword' className='form-label'>
+            <div className="mb-3">
+              <label htmlFor="editConfirmPassword" className="form-label">
                 Confirm Password
               </label>
               <input
-                type='password'
-                className='form-control'
-                id='confirmPassword'
+                type="password"
+                className="form-control"
+                id="editConfirmPassword"
                 value={currentUser?.confirmPassword}
               />
             </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant='secondary' onClick={handleCloseModal}>
+          <Button variant="secondary" onClick={handleCloseModal}>
             Close
           </Button>
-          <Button variant='primary' onClick={handleEditUser}>
+          <Button variant="primary" onClick={handleEditUser}>
             Save Changes
           </Button>
         </Modal.Footer>
